ea8ac69a2ab7fcb790496d6f1a80d73d4a03f88e
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-01-17 09:40:30 +0000

    Tecltas alfabetic fet

--- Ejemplos/Ejemplo7: Teclado Num/Ejercicio1.js
+++ Ejemplos/Ejemplo7: Teclado Num/Ejercicio1.js
@@ -1,9 +1,14 @@
-for (i = 1; i < 31; i++) {
+/* EXERCICI 1 */
+for (i = 0; i < 10; i++) {
     let num = document.createTextNode(i)
     let cuadrado = document.createElement('p')
     cuadrado.appendChild(num)
     let teclado = document.getElementById('contenedor')
-    
+    estilosNumerico(cuadrado, teclado);
+    teclado.appendChild(cuadrado)
+}
+
+function estilosNumerico(cuadrado, teclado) {
     cuadrado.style.color = 'white'
     cuadrado.style.backgroundColor = '#008B8B'
     cuadrado.style.height = '50px'
@@ -17,9 +22,7 @@ for (i = 1; i < 31; i++) {
     teclado.style.gridTemplateColumns = 'auto auto auto'
     teclado.style.grid = '3'
     teclado.style.width = '100px'
-    
-    teclado.appendChild(cuadrado)
-    
+
     if (i % 3 == 0) {
         cuadrado.style.backgroundColor = '#FED801';
     }
@@ -27,5 +30,37 @@ for (i = 1; i < 31; i++) {
     if (i % 2 == 0) {
         cuadrado.style.backgroundColor = 'blue'
     }
+}
 
+/* EXERCICI 2 */
+for (i = 1; i < 2; i++) {
+    for (j = 65; j < 91; j++) {
+        let letra = String.fromCharCode(j)
+        let letras = document.createTextNode(letra)
+        let cuadro = document.createElement('p')
+        cuadro.appendChild(letras)
+        let teclado = document.getElementById('contenedor2')
+        estilosAlfabetico(cuadro, teclado, letra);
+        teclado.appendChild(cuadro)
+    }
+}
+
+function estilosAlfabetico(cuadro, teclado, letra) {
+    cuadro.style.color = 'white'
+    cuadro.style.backgroundColor = '#FF8C00'
+    cuadro.style.height = '50px'
+    cuadro.style.width = '50px'
+    cuadro.style.fontSize = '45px'
+    cuadro.style.textAlign = 'center'
+    cuadro.style.margin = '5px'
+    cuadro.style.borderRadius = '5px'
+
+    teclado.style.display = 'grid'
+    teclado.style.gridTemplateColumns = 'auto auto auto auto auto auto auto auto'
+    teclado.style.grid = '3'
+    teclado.style.width = '100px'
+
+    if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O'|| letra == 'U') {
+        cuadro.style.backgroundColor = '#008000'
+    }
 }
---
+++ Ejemplos/Ejemplo8: Funciones/Ejercicio1.js
@@ -0,0 +1,8 @@
+const sayHi = () => console.log ("Hello there!");
+const askAboutDay = () => 
+    setTimeout (() => console.log ("How was your day?"), 4000);
+const sayBye = () => console.log ("See you later!");
+
+sayHi();
+askAboutDay();
+sayBye();
