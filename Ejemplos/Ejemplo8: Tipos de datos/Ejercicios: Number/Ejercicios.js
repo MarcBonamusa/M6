f203e8eadebbc929fb7432ebec2911fa08f2c9de
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-01-27 08:21:05 +0000

    Todos hechos menos el 5 de numbers

--- Ejemplos/Ejemplo8: Tipos de datos/Ejercicios: Number/Ejercicios.js
+++ Ejemplos/Ejemplo8: Tipos de datos/Ejercicios: Number/Ejercicios.js
@@ -1,6 +1,7 @@
 let num1 = prompt("Introduce un número");
-let num2 = prompt("Introduce otro número");
+let num2 = prompt ("Introduce otro número");
 
+// Ejercicio 1
 function mayor(num1, num2) {
     if (num1 > num2) {
         return num1;
@@ -13,9 +14,9 @@ alert(`El número mayor es ${mayor(num1, num2)}`);
 
 
 // Ejercicio 2
-
-
-function multiplicar(num1, num2) {
+function multiplicar() {
+    let num1 = parseInt (prompt("Introduce un número"));
+    let num2 = parseInt (prompt ("Introduce otro número"));
     let resultado = 0;
     for (let i = 0; i < num2; i++) {
         resultado += num1;
@@ -23,8 +24,30 @@ function multiplicar(num1, num2) {
     return resultado;
 }
 
-alert(`El resultado de la multiplicacion es: ${multiplicar(num1, num2)}`);
+alert(`El resultado de la multiplicacion es: ${multiplicar()}`);
 
+// Ejercicio 3
+function multiTresNum() {
+    let num1 = parseInt(prompt("Introduce un número"));
+    let num2 = parseInt(prompt("Introduce otro número"));
+    let num3 = parseInt(prompt("Introduce otro número más"));
+    
+    let resultado = 0;
+    for (let i = 0; i < num2; i++) {
+        resultado += num1;
+    }
+
+    let resultadoFinal = 0;
+    for (let i = 0; i < num3; i++) {
+        resultadoFinal += resultado;
+    }
+
+    return resultadoFinal;
+}
+
+alert(`El resultado de la multiplicación es: ${multiTresNum()}`);
+
+// Ejercicio 4
 let nota1 = parseFloat(prompt("Introduce la primera nota"));
 let nota2 = parseFloat(prompt("Introduce la segunda nota"));
 let nota3 = parseFloat(prompt("Introduce la tercera nota"));
@@ -35,6 +58,26 @@ function notaMedia(nota1, nota2, nota3) {
 
 alert(`El resultado de la media de las notas es: ${notaMedia(nota1, nota2, nota3).toFixed(2)}`);
 
+// Ejercicio 5
+function esNumeroArmstrong(num1) {
+
+}
+
+// Ejercicio 6
+function alCubo(num1) {
+    let resultado = Math.pow(num1, 3)
+    let estado = true
+    if (Number.isInteger(resultado)) {
+        alert (`${resultado} es un numero entero`)
+    } else {
+        estado = false
+        alert (`${resultado} no es un numero entero`)
+    }
+}
+
+alCubo(num1)
+
+// Ejercicio Dados
 let dado1 = Math.floor(Math.random() * 6) + 1;
 let dado2 = Math.floor(Math.random() * 6) + 1;
 
--- Ejemplos/Ejemplo8: Tipos de datos/Ejercicios: Strings/Ejercicios.js
+++ Ejemplos/Ejemplo8: Tipos de datos/Ejercicios: Strings/Ejercicios.js
@@ -1,6 +1,6 @@
-
 let str = prompt("Introduce una palabra");
-
+/*
+// Ejercicio 1
 function conAs(str) {
     if (str.startsWith('As') || str.startsWith('as')) {
         return str;
@@ -11,6 +11,7 @@ function conAs(str) {
 
 alert(`El string es: ${conAs(str)}`);
 
+// Ejercicio 2
 let pos = parseInt(prompt("Introduce un número"));
 
 function eliminaCaracterPosicion(str, pos) {
@@ -23,6 +24,7 @@ function eliminaCaracterPosicion(str, pos) {
 
 alert(`El string es: ${eliminaCaracterPosicion(str, pos)}`);
 
+// Ejercicio 3
 function cambiar(str) {
     if (str.length > 0) {
         return str.charAt(str.length - 1) + str.slice(1, -1) + str.charAt(0);
@@ -32,3 +34,21 @@ function cambiar(str) {
 }
 
 alert(`El string intercambiado es: ${cambiar(str)}`);
+*/
+// Ejercicio 4
+function separarVocalesDeConsonantes(str) {
+    let vocales = '';
+    let consonantes = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        if ('aeiouAEIOU'.includes(str[i])) {
+            vocales += str[i];
+        } else if (/[a-zA-Z]/.test(str[i])) {
+            consonantes += str[i];
+        }
+    }
+    
+    return vocales + consonantes;
+}
+
+alert (`Resultado: ${separarVocalesDeConsonantes(str)}`)
