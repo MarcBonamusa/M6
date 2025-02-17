a48c70bcc6c936ef0c19c304d05aaed1aa617c45
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-02-17 09:45:22 +0000

    Acabar ejercicio 2

--- Ejemplos OBJ-JSON/Ejemplo1: Objetos/Ejemplo_alumno.js
+++ Ejemplos OBJ-JSON/Ejemplo1: Objetos/Ejemplo_alumno.js
@@ -17,7 +17,7 @@ let valor = prompt("Introduce el valor del campo", "1ero")
 alumno2[propiedad] = valor
 
 console.log(alumno2)
-*/
+
 
 function printAlumno(alumno2) {
     var result = ``;
@@ -26,17 +26,69 @@ function printAlumno(alumno2) {
     }
     return result
 }
-console.log(printAlumno(alumno2))
-
-//nombre = Aina
-//apellidos = Garcia López
-//edad = 21
-//curso = DAMv1
+console.log(printAlumno(alumno2))//nombre = Aina apellidos = Garcia López edad = 21 curso = DAMv1
 
 
 alumno2.getInfo = function() {
     return 'El alumno ' + this.nombre + ' ' + this.apellidos + ' tiene ' + this.edad + ' años'
 }
-console.log(alumno2.getInfo());
+console.log(alumno2.getInfo()); // El alumno Aina Garcia López tiene 21 años
 
-// El alumno Aina Garcia López tiene 21 años
+// ¿Existe la propiedad?
+console.log(alumno2.notas); //undefined
+console.log(alumno2.notas?.m4); //undefined
+console.log(alumno2.hasOwnProperty("apellidos")); //true
+console.log("edad" in alumno2); //true
+console.log("fecha nacimiento" in alumno2); //false
+let p = "telefono2";
+console.log(p in alumno2); //true
+
+
+// Más métodos
+console.log(Object.keys(alumno2))
+console.log(Object.values(alumno2))
+console.log(Object.entries(alumno2))
+
+
+const alumno3 = {
+    name: "Manz",
+    life: 99,
+    power: 10,
+    talk: function() {
+      return "Hola!";
+    }
+};
+
+console.log(Object.keys(alumno3))
+console.log(Object.values(alumno3))
+console.log(Object.entries(alumno3))
+
+const keys = ["name", "life", "power", "talk"];
+const values = ["Manz", 99, 10, function() { return "Hola" }];
+const entries = [];
+
+for (let i of Object.keys(keys)) {
+    const key = keys[i];
+    const value = values[i];
+    entries.push([key, value]);
+    console.log(keys[i])
+}
+
+const alumno4 = Object.fromEntries(entries);
+*/
+
+let employees = `[
+    { "firstName":"John", "lastName":"Doe" },
+    { "firstName":"Anna", "lastName":"Smith" },
+    { "firstName":"Peter", "lastName":"Jones" }
+]`;
+let object = JSON.parse(employees); // Adapta el JSON a un objecte
+console.log(object)
+
+let employees2 = [
+    { "firstName":"John", "lastName":"Doe" },
+    { "firstName":"Anna", "lastName":"Smith" },
+    { "firstName":"Peter", "lastName":"Jones" }
+];
+let object2 = employees2;
+console.log(object2)
--- Ejemplos OBJ-JSON/Ejemplo1: Objetos/Ejercicio1.js
+++ Ejemplos OBJ-JSON/Ejemplo1: Objetos/Ejercicio1.js
@@ -1,3 +1,4 @@
+//Ejercicio 1
 var tvSamsung = {
     nombre: "TV Samsung 42”",
     categoria: "Televisores",
@@ -7,6 +8,39 @@ var tvSamsung = {
     getImporte: function() {
     return this.unidades * this.precio
     }
-};
 
+};
 console.log(tvSamsung.getImporte())
+
+//Ejercicio 2
+/*
+for(let i = 1; i<=3; i++) {
+    tvSamsung["Resolucion" + i] = prompt("Resolucion" + i, "full HD")
+}
+
+const propiedades = ["resolucion", "colores"];
+
+// ¿Existe la propiedad?
+if ("resolucion" in tvSamsung === false) {
+    let valor = prompt("Introduce la resolucion", "default value")
+    tvSamsung["resolucion"] = valor
+}
+
+if ("colores" in tvSamsung === false) {
+    let valor = prompt("Introduce el tipo de colores", "default value")
+    tvSamsung["colores"] = valor
+}
+
+document.getElementById("precio").textContent = tvSamsung.getImporte();
+*/
+//Ejercicio 3
+let tvSamsung2 = `[
+    { "marca":"LG", "categoria":"Televisores" },
+    { "marca":"Samsung", "categoria":"Televisores" },
+    { "marca":"Xiaomi", "categoria":"Televisores" }
+]`;
+let string = JSON.stringify(tvSamsung2);
+console.log(string)
+
+let inverso = JSON.parse(tvSamsung2);
+console.log(inverso)
