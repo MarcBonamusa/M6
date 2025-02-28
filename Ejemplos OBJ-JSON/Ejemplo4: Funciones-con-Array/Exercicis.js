ac20b42908f2dd82c373c39c12eae998b5f87e04
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-02-28 09:44:56 +0000

    2 exercicis fets

---
+++ Ejemplos OBJ-JSON/Ejemplo4: Funciones-con-Array/Exercicis.js
@@ -0,0 +1,56 @@
+// Todos
+// EXERCICI 1
+const miArray = ['hola', 'que', 'tal', 'estas', 'tu', 'bien', 'gracias'];
+
+function todosSonStrings(miArray) {
+    return miArray.every(palabra => typeof palabra === 'string');
+}
+
+console.log("EXERCICI 1");
+console.log(todosSonStrings(miArray));
+  
+// EXERCICI 2
+const miArray2 = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+
+function todosSonPares(miArray2) {
+    return miArray2.every(numero => numero % 2 === 0);
+}
+
+console.log("EXERCICI 2");
+console.log(todosSonPares(miArray2));
+
+// Al menos uno
+// EXERCICI 3
+const miArray3 = ['hola', 'que', 'tal', 'estas', 'tu', 'bien', 'gracias'];
+
+function longitudMayorA2 (miArray3) {
+    
+}
+
+// EXERCICI 4
+
+// Transforma
+// EXERCICI 5
+
+// EXERCICI 6
+
+// Filtro
+// EXERCICI 7
+
+// EXERCICI 8
+
+// Busca
+// EXERCICI 9
+
+// EXERCICI 10
+
+// EXERCICI 11
+
+// EXERCICI 12
+
+// Acumula
+// EXERCICI 13
+
+// EXERCICI 14
+
+// EXERCICI 15
