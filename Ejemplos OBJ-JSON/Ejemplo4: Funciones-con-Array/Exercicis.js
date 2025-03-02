a2d20a67728c714489c6d4a5a2045dfb2b18d52a
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-03-02 16:33:22 +0000

    5 exercicis mes fets

--- Ejemplos OBJ-JSON/Ejemplo4: Funciones-con-Array/Exercicis.js
+++ Ejemplos OBJ-JSON/Ejemplo4: Funciones-con-Array/Exercicis.js
@@ -86,14 +86,47 @@ console.log(stringLengths(miArray5));
 // EXERCICI 9
 
 // EXERCICI 10
+const miArray10 = ['hola', 'que', 'tal', 'estas', 'tu', 'bien', 'gracias', 'pues'];
+
+function filtrarUltimaLetra(miArray10){
+    return miArray10.reverse().find(palabra => palabra.startsWith('p'))
+}
+
 
 // EXERCICI 11
+const miArray11 = ['hola', 'que', 'tal', 'estas', 'tu', 'bien', 'gracias', 'tambien'];
+
+function filtrarPosicion(miArray11){
+    return miArray11.findIndex(palabra => palabra.startsWith('t'))
+}
 
 // EXERCICI 12
+const miArray12 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function ultimoPar(miArray12){
+    return miArray12.reverse().find(numPar => numPar % 2 == 0)
+}
 
 // Acumula
 // EXERCICI 13
+const miArray13 = [1, 2, 3, 4, 5, 6, 7]
+
+function sumaArray(miArray13){
+    let sumaFinal = 0 
+    miArray13.forEach(num => { sumaFinal = num + sumaFinal });
+    return sumaFinal
+}
 
 // EXERCICI 14
+const miArray14 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function restaArray(miArray14){
+    return miArray14.reduce((acumulador, num) => acumulador - num);
+}
 
 // EXERCICI 15
+const miArray15 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function restaElemento(miArray15){
+    return miArray15.reverse().reduce((acumulador, num) => acumulador - num);
+}
