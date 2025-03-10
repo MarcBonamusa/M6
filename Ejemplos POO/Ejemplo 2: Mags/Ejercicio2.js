b0fe0aae89862337280c9a365c5cbf711db91a46
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-03-10 08:13:41 +0000

    Ejercicios Personajes hecho

--- Ejemplos POO/Ejemplo 2: Mags/Ejercicio2.js
+++ Ejemplos POO/Ejemplo 2: Mags/Ejercicio2.js
@@ -1,8 +1,75 @@
-import { Guerrero } from "./Guerrero";
-import { Mago } from "./Mago";
+class Personaje {
+    nombre;
+    nivel;
+    puntosDeVida;
 
-const mago1 = new Mago ()
-const mago2 = new Mago ()
+    constructor(nombre, nivel, puntosDeVida) {
+        this.nombre = nombre
+        this.nivel = nivel
+        this.puntosDeVida = puntosDeVida
+    }
 
-const guerrero1 = new Guerrero ()
-const guerrero2 = new Guerrero ()
+    valueOf() { return this.nivel }
+
+    atacar(daño) { return `${this.nombre} ataca y hace ${daño} de daño` }
+}
+
+class Guerrero extends Personaje {
+    fuerza;
+
+    constructor(nombre, nivel, puntosDeVida, fuerza) {
+        super (nombre, nivel, puntosDeVida) 
+        this.fuerza = fuerza
+    }
+
+    golpeEspada(daño) { return `El guerrero ${this.nombre} ataca con la espada y hace ${daño} de daño` }
+}
+
+class Mago extends Personaje {
+    mana;
+
+    constructor(nombre, nivel, puntosDeVida, mana) {
+        super (nombre, nivel, puntosDeVida) 
+        this.mana = mana
+    }
+
+    lanzarHechizo() { return `El mago ${this.nombre} lanza un hechizo de ${this.mana}` }
+}
+
+let mago1 
+let mago2
+let guerrero1
+let guerrero2
+
+let listaPersonajes = [
+    mago1 = new Mago ("Alvaro", 1, 130, 20),
+    mago2 = new Mago ("Koke", 8, 35, 10),
+    guerrero1 = new Guerrero ("Dakis Bu", 3, 150, 30),
+    guerrero2 = new Guerrero ("Guillermo", 10, 30, 10)
+]
+
+console.log("-- MAGOS --")
+// MAGO 1
+console.log(mago1)
+console.log(mago1.atacar(20))
+console.log(mago1.lanzarHechizo())
+
+// MAGO 2
+console.log(mago2)
+console.log(mago2.atacar(50))
+console.log(mago2.lanzarHechizo())
+
+console.log("\n-- GUERREROS --")
+// GUERRERO 1
+console.log(guerrero1)
+console.log(guerrero1.atacar(50))
+console.log(guerrero1.golpeEspada(70))
+
+// GUERRERO 2
+console.log(guerrero2)
+console.log(guerrero2.atacar(20))
+console.log(guerrero2.golpeEspada(40))
+
+console.log("\n-- LISTA ORDENADA --")
+let listaPersonajesOrdenada = listaPersonajes.sort((a, b) => a - b)
+console.log(listaPersonajesOrdenada)
