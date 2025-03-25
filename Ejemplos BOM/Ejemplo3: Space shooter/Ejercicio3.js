d3436eec684d405146544410806a9820ba1d1c96
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-03-25 16:39:03 +0000

    Space shooter hecho

--- Ejemplos BOM/Ejemplo3: Space shooter/Ejercicio3.js
+++ Ejemplos BOM/Ejemplo3: Space shooter/Ejercicio3.js
@@ -4,8 +4,31 @@ document.addEventListener('mousemove', (event) => {
     foto.style.right = `${event.clientY}px`
 });
 
-document.addEventListener("keypress", () =>{
+document.addEventListener("keypress", (event) => {
     if (event.key === " ") {
-        console.log("ESPACIO")
+        dispararBala();
     }
-})
+});
+
+function dispararBala() {
+    const foto = document.getElementById('foto');
+    const bala = document.createElement("div");
+    bala.classList.add("bala");
+    document.body.appendChild(bala);
+    
+    let x = foto.offsetLeft + foto.width / 2;
+    let y = foto.offsetTop;
+    
+    bala.style.left = `${x}px`;
+    bala.style.top = `${y}px`;
+
+    let intervalo = setInterval(() => {
+        y -= 5;
+        bala.style.top = `${y}px`;
+        if (y < 0) {
+            clearInterval(intervalo);
+            bala.remove();
+        }
+    }, 20);
+
+}
